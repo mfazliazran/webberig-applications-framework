d1fe13720c5e4a566e20ede95fd7e7ebaeacfbc8
mathieu <mathieu@webberig.be>
2011-08-19 19:07:57 +0000

    Integrating the 0.2 changes to the trunk

--- application/modules/account/changepassword.php
+++ application/modules/account/changepassword.php
@@ -1,97 +1,70 @@
 <?php
 if (!$p)
 {
-	$header = new Header("Paswoord wijzigen","site/images/icons/changepassword.png") ;
-	$this->LoadLibrary("waf_forms");
+    $header = new Header("Paswoord wijzigen","site/images/icons/changepassword.png") ;
+    $this->LoadLibrary("waf_forms");
 
-	$form = new Waf_Form();
-	$form->buttonLabel = "Verzenden";
+    $form = new Waf_Form();
+    $form->buttonLabel = "Verzenden";
 
-	/////////////////////////////////////////////////////
-	// 1. Definition
-	/////////////////////////////////////////////////////
-	//Create inputs
-	///////////////////////////////////////////////
-	
-	$input = $form->CreateInput("Password", "old");
-	$input->label = "Oud wachtwoord";
-	
-	$input = $form->CreateInput("Password", "new");
-	$input->label = "Nieuw wachtwoord";
+    /////////////////////////////////////////////////////
+    // 1. Definition
+    /////////////////////////////////////////////////////
+    //Create inputs
+    ///////////////////////////////////////////////
 
-	$input = $form->CreateInput("Password", "new2");
-	$input->label = "Herhaal nieuw wachtwoord";
+    $input = $form->CreateInput("Password", "old");
+    $input->label = "Oud wachtwoord";
 
-	//Create validators
-	///////////////////////////////////////////////
-	$validator = $form->CreateValidator("Equal", "new");
-	$validator->second_field = "new2";
-	$validator->message = "Nieuwe wachtwoorden zijn niet gelijk";
+    $input = $form->CreateInput("Password", "new");
+    $input->label = "Nieuw wachtwoord";
 
-	$validator = $form->CreateValidator("Required", "old");
-	$validator->message = "Oud wachtwoord is verplicht";
+    $input = $form->CreateInput("Password", "new2");
+    $input->label = "Herhaal nieuw wachtwoord";
 
-	$validator = $form->CreateValidator("Required", "new");
-	$validator->message = "Nieuw wachtwoord is verplicht";
+    //Create validators
+    ///////////////////////////////////////////////
+    $validator = $form->CreateValidator("Equal", "new");
+    $validator->compareWith = "new2";
+    $validator->message = "Paswoorden komen niet overeen";
 
-	$validator = $form->CreateValidator("Required", "new2");
-	$validator->message = "U moet het wachtwoord herhalen";
+    $validator = $form->CreateValidator("Required", "old");
+    $validator->message = "Oud wachtwoord is verplicht";
+
+    $validator = $form->CreateValidator("Required", "new");
+    $validator->message = "Nieuw wachtwoord is verplicht";
+
+    $validator = $form->CreateValidator("Required", "new2");
+    $validator->message = "U moet het wachtwoord herhalen";
+
+    $validator = $form->CreateValidator("Password", "new");
+    $validator->message = "Het paswoord is niet sterk genoeg";
 
 
-	/////////////////////////////////////////////////////
-	// 2. Handling
-	/////////////////////////////////////////////////////
-	$form->ProcessForm();
-	
-	if ($form->isProcessed())
-	{
-		Users::ChangePassword($form);
-		$this->DoRedirect(".");
-	}
-	/*
-	if (count($_POST)>0)
-	{
-		$errors = array();
-		$form = $_POST;
+    /////////////////////////////////////////////////////
+    // 2. Handling
+    /////////////////////////////////////////////////////
+    $form->ProcessForm();
 
-		//VALIDATION
-		if(Validator::Required($form['old'], &$errors, "Gelieve uw oud wachtwoord in te vullen"))
-		{
-			if (!Security::Login($_SESSION['user'], $form['old']))
-			{
-				$errors[count($errors)] = "Het wachtwoord dat u heeft opgegeven is niet juist";
-			}
-		}
-
-		Validator::Required($form['new'], &$errors, "Nieuw wachtwoord is verplicht");
-		Validator::Required($form['new2'], &$errors, "Nieuw wachtwoord is verplicht");
-		Validator::IsEqual($form['new'], $form['new2'], &$errors, "Nieuwe wachtwoorden zijn niet gelijk");
-
-		if (count($errors)==0)
-		{
-			//VALIDATED
-			if (!Users::Changepassword($_SESSION['userID'], $form['new']))
-			{
-				$errors[0] = "Er heeft zich een ongekende fout voorgedaan;";			
-			} else
-			{
-				header("location: home.html");
-			}
-			
-		}				
-	}
-	*/
+    if ($form->isProcessed())
+    {
+        print_r($form->ToArray());
+        print_r($_SESSION);
+        die();
+        Users::ChangePassword($form);
+        $this->DoRedirect(".");
+    }
 } else {
 
-	/////////////////////////////////////////////////////
-	// 3. Show errors if any!
-	/////////////////////////////////////////////////////
-	$form->ShowErrors();
-	
-	/////////////////////////////////////////////////////
-	// 4. Show form
-	/////////////////////////////////////////////////////
-	$form->ShowForm();
+    /////////////////////////////////////////////////////
+    // 3. Show errors if any!
+    /////////////////////////////////////////////////////
+    $form->ShowErrors();
+
+    /////////////////////////////////////////////////////
+    // 4. Show form
+    /////////////////////////////////////////////////////
+    $form->ShowForm();
 
 }
 ?>
