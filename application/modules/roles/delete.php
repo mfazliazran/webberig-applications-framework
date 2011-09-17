c69f641b5e79681a5aeec257f6bc5a59954a816b
mathieu <mathieu@webberig.be>
2011-09-17 17:14:58 +0000

    Completed I18n code, translated Account module and applets

--- application/modules/roles/delete.php
+++ application/modules/roles/delete.php
@@ -4,7 +4,7 @@ if (!$p)
     if (isset($_GET['value'])&&is_numeric($_GET['value']))
     {
         Roles::Delete($_GET['value']);	
-        Messenger::Add("confirm", "De rol is met succes verwijderd.");
+        Messenger::Add("confirm", _("Role has been deleted successfully"));
     }
 }
 $this->DoRedirect("roles");
